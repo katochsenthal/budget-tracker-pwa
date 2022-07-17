ca6de2cafcf2f57beeeb323c3c6b944d2a9b0c48
katochsenthal <katoch.senthal@gmail.com>
2022-07-16 21:11:47 -0400

    fixed offline functionality

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,87 +1,53 @@
-const CACHE_NAME = "budget-tracker-v1";
-const DATA_CACHE_NAME = "data-cache-v1";
-
-const FILES_TO_CACHE = [
+const APP_PREFIX = "budget-tracker-pwa";
+const VERSION = "0.0.1.3";
+const CACHE_NAME = APP_PREFIX + VERSION;
+const URLS = [
+  "./",
   "./index.html",
-  "./manifest.json",
-  "./css/styles.css",
-  "./icons/icon-72x72.png",
-  "./icons/icon-96x96.png",
-  "./icons/icon-128x128.png",
-  "./icons/icon-144x144.png",
-  "./icons/icon-152x152.png",
-  "./icons/icon-192x192.png",
-  "./icons/icon-384x384.png",
-  "./icons/icon-512x512.png",
-  "./js/idb.js",
   "./js/index.js",
+  "./js/idb.js",
+  "./css/styles.css",
+  "./manifest.json",
+  "./service-worker.js",
 ];
+self.addEventListener("install", (e) => e.waitUntil(swInstall()));
+self.addEventListener("activate", (e) => e.waitUntil(swActivate()));
+self.addEventListener("fetch", (e) => e.respondWith(swFetch(e)));
 
-// Intercept fetch requests
-self.addEventListener("fetch", function (evt) {
-  if (evt.request.url.includes("/api/")) {
-    evt.respondWith(
-      caches
-        .open(DATA_CACHE_NAME)
-        .then((cache) => {
-          return fetch(evt.request)
-            .then((response) => {
-              // If the response was good, clone it and store it in the cache.
-              if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
-              }
+async function swFetch(e) {
+  console.log("sw[fetch]");
+  let request = await caches.match(e.request);
+  return request || fetchAndCache(e.request);
+}
 
-              return response;
-            })
-            .catch((err) => {
-              // Network request failed, try to get it from the cache.
-              return cache.match(evt.request);
-            });
-        })
-        .catch((err) => console.log(err))
-    );
+async function swInstall() {
+  console.log("sw[install]");
+  const cache = await caches.open(CACHE_NAME);
+  let options = { headers: { "cache-control": "no-cache" } };
+  let requests = URLS.map((url) => new Request(url, options));
+  console.log(requests);
+  await cache.addAll(requests);
+  await self.skipWaiting();
+}
 
-    return;
-  }
-
-  evt.respondWith(
-    fetch(evt.request).catch(function () {
-      return caches.match(evt.request).then(function (response) {
-        if (response) {
-          return response;
-        } else if (evt.request.headers.get("accept").includes("text/html")) {
-          // return the cached home page for all requests for html pages
-          return caches.match("/");
-        }
-      });
+async function swActivate() {
+  console.log("sw[activate]");
+  let keyList = await caches.keys();
+  let cacheWhitelist = keyList.filter((key) => key.indexOf(APP_PREFIX));
+  cacheWhitelist.push(CACHE_NAME);
+  return Promise.all(
+    keyList.map(function (key, i) {
+      if (cacheWhitelist.indexOf(key) === -1) {
+        console.log("deleting cache : " + keyList[i]);
+        return caches.delete(keyList[i]);
+      }
     })
   );
-});
+}
 
-// Cache resources
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log("Your files were pre-cached successfully!");
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-});
-
-// Activate the service worker and remove old data from the cache
-self.addEventListener("activate", function (evt) {
-  evt.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log("Removing old cache data", key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-
-  self.clients.claim();
-});
+async function fetchAndCache(request) {
+  const res = await fetch(request);
+  const cache = await caches.open(CACHE_NAME);
+  cache.put(request, res.clone());
+  return res;
+}
